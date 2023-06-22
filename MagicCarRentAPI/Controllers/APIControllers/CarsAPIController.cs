4b42168aae791cfaddb43a3e9ec8103cec36d425
Pe6e3 <xpe6e3@gmail.com>
2023-06-23 01:30:14 +0600

    Создал Post методы по созданию авто и записи в журнале

---
+++ MagicCarRentAPI/Controllers/APIControllers/CarsAPIController.cs
@@ -0,0 +1,33 @@
+﻿using MagicCarRentAPI.Data;
+using MagicCarRentAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MagicCarRentAPI.Controllers.APIControllers
+{
+    [Route("api/Cars")]
+    [ApiController]
+    public class CarsAPIController : ControllerBase
+    {
+        private readonly AppDbContext db;
+        public CarsAPIController(AppDbContext db)
+        {
+            this.db = db;
+        }
+
+        // GET: api/<CarsAPIController>
+        [HttpGet]
+        public async Task<IActionResult> GetCars() => Ok(await db.Cars.ToListAsync());
+
+        [HttpPost]
+        public async Task<IActionResult> AddCar(Car car)
+        {
+            db.Add(car);
+            await db.SaveChangesAsync();
+
+            return Ok(car);
+        }
+    }
+}
--- MagicCarRentAPI/Controllers/APIControllers/JournalAPIController.cs
+++ MagicCarRentAPI/Controllers/APIControllers/JournalAPIController.cs
@@ -1,5 +1,6 @@
 ﻿using MagicCarRentAPI.Data;
 using MagicCarRentAPI.Entities;
+using MagicCarRentAPI.Entities.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,6 +16,7 @@ public class JournalAPIController : ControllerBase
         this.db = db;
     }
 
+
     [HttpGet]
     public async Task<IActionResult> GetJournal() => Ok(await db.Journal.Include("Car").ToListAsync());
 
@@ -22,6 +24,44 @@ public class JournalAPIController : ControllerBase
     public async Task<IActionResult> GetLineJournal(int id) => Ok(await db.Journal.FirstOrDefaultAsync(j => j.Id == id));
 
     [HttpGet("{carModel}")]
-    public async Task<IActionResult> GetLinesByCar([FromRoute] string carModel) => Ok(await db.Journal.Include(x => x.Car).Where(x => x.Car.Model == carModel).ToListAsync());
+    public async Task<IActionResult> GetLinesByCar([FromRoute] string carModel)
+    {
+        var journal = await db.Journal.Include(x => x.Car).Where(x => x.Car.Model == carModel).ToListAsync();
+        if (journal.Count == 0) return BadRequest();
+        return Ok(journal);
+    }
 
+    [HttpPost]
+    public async Task<IActionResult> AddJournalLine(int clientID, int carID, DateTime beginRent, DateTime endRent)
+    {
+        Journal journal = new Journal()
+        {
+            ClientID = clientID,
+            CarID = carID,
+            BeginRent = beginRent,
+            EndRent = endRent
+        };
+
+        db.Add(journal);
+        await db.SaveChangesAsync();
+        return Ok(journal);
+    }
+
+    //public async Task<IActionResult> AddJournalLine(int clientID, int carID, DateTime beginRent, DateTime endRent)
+    //{
+    //    JournalDTO journal = new JournalDTO()
+    //    {
+    //        ClientID = clientID,
+    //        CarID = carID,
+    //        BeginRent = beginRent,
+    //        EndRent = endRent
+    //    };
+    //    using (HttpClient client = new HttpClient())
+    //    {
+    //        client.BaseAddress = new Uri("https://localhost:7067/Journal/Create/");
+
+    //        var response = await client.PostAsJsonAsync("Journal/Create", journal);
+    //        return Ok(journal);
+    //    }
+    //}
 }
